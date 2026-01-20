48687c39c186d13d5433393900f052f57f80502c
martintricaud <martin.tricaud@gmail.com>
2026-01-20 14:33:17 +0100

    adds keypress handlers to trigger actions sequentially from a cue list

--- unitree-go2-gui/src/App.svelte
+++ unitree-go2-gui/src/App.svelte
@@ -20,6 +20,22 @@
     import { gamepadStore } from './lib/gamepadStore';
     ThemeUtils.setGlobalDefaultTheme(ThemeUtils.presets.retro);
 
+    type TypeMap = {
+        number: number;
+        string: string;
+        boolean: boolean;
+        // Add other types as needed
+    };
+
+    type CmdJson = {
+        keyName: string;
+        valueType: keyof TypeMap; // This will be "number", "string", "boolean", etc.
+    };
+
+    type GenerateTypeFromArray<T extends CmdJson[]> = {
+        [K in T[number] as K['keyName']]: TypeMap[K['valueType']];
+    };
+
     type CommandArg = {
         name: string;
         type: string;
@@ -27,16 +43,12 @@
 
     type Command = {
         command_name: string;
-        api_code: number;
+        api_id: number;
         description?: string;
-        args?: CommandArg[];
         working: boolean;
         exec_time: number;
         topic: string;
         gamepad_assignment: string;
-    };
-
-    type ParametrizedCommand = Command & {
         args: CommandArg[];
     };
 
@@ -53,9 +65,65 @@
     let MONITOR_STATE = true;
     let FACE_DETECTION = false;
     let FACE_LANDMARKS = false;
+    let GENDER_PREDICTION = false;
+    let AGE_PREDICTION = false;
     let buttonsStore = derived(gamepadStore, ($gamepadStore) => $gamepadStore.buttonsState);
     let axisStore = derived(gamepadStore, ($gamepadStore) => $gamepadStore.axisState);
 
+    let indexByCommandName: (list: Command[]) => Record<string, Command> = R.indexBy(
+        R.prop('command_name'),
+    );
+    let commandIndex = indexByCommandName(commands as Command[]);
+
+    ///utility for generating a JSON message containing the api_id and topic fields
+    let buildPayload = (cmd: string) => R.pick(['api_id', 'topic'], commandIndex[cmd]);
+    ///utility for generating "toggle switch on" JSON messages
+    let switchTo = (newState: boolean) => (cmd: string) => {
+        return { parameter: R.objOf(commandIndex[cmd].args[0].name, newState) };
+    };
+
+    let stepCounter = 0;
+    let actionSequence = [
+        { command: 'Backstand', ...switchTo(true)('Backstand'), ...buildPayload('Backstand') },
+        {
+            command: 'ClassicWalk',
+            ...switchTo(true)('ClassicWalk'),
+            ...buildPayload('ClassicWalk'),
+        },
+        {
+            sequence: [
+                { command: 'Stretch', ...buildPayload('Stretch') },
+                { command: 'Hello', ...buildPayload('Hello') },
+                { wait: 0.3 },
+                { command: 'FrontJump', ...buildPayload('FrontJump') },
+                { wait: 1 },
+                { command: 'FrontJump', ...buildPayload('FrontJump') },
+                { command: 'ClassicWalk', ...switchTo(true)('ClassicWalk'), ...buildPayload('ClassicWalk'),},
+            ],
+        },
+        { command: 'Pose', ...switchTo(true)('Pose'), ...buildPayload('Pose') },
+        { command: 'Handstand', ...switchTo(true)('Handstand'), ...buildPayload('Handstand') },
+        {
+            sequence: [
+                {
+                    command: 'ClassicWalk',
+                    ...switchTo(true)('ClassicWalk'),
+                    ...buildPayload('ClassicWalk'),
+                },
+                { wait: 1 },
+                { command: 'FingerHeart', ...buildPayload('FingerHeart') },
+            ],
+        },
+
+        { command: 'Sit', ...buildPayload('Sit') },
+        { command: 'RiseSit', ...buildPayload('RiseSit') },
+        { command: 'Jumping', ...switchTo(true)('Jumping'), ...buildPayload('Jumping') },
+    ];
+
+    let execute_step = (_actionSequence: any) => (i: number) => {
+        console.log(_actionSequence[i]);
+        websocket.send(_actionSequence[i]);
+    };
     //create an object where each key is a button name and the value is a derived store for that button's state
     // let arrayOfButtonStores = R.mapObjIndexed(
     //     (value, key) => derived(buttonsStore, (state) => state[key]),
@@ -73,7 +141,7 @@
     //             if (buttonState.length >= 2 && R.last(buttonState).value == false) {
     //                 websocket.send({
     //                     command: 'action',
-    //                     api_id: command.api_code,
+    //                     api_id: command.api_id,
     //                     topic: command.topic,
     //                 });
     //             }
@@ -82,30 +150,19 @@
     // )
 
     let colors = ['🤍 white', '❤️ red', '💛 yellow', '💙 blue', '💚 green', '🩵 cyan', '💜 purple'];
-    let color:
-        | '🤍 white'
-        | '❤️ red'
-        | '💛 yellow'
-        | '💙 blue'
-        | '💚 green'
-        | '🩵 cyan'
-        | '💜 purple' = '🤍 white';
     let movementMode = '';
     let speedLevel: 'Slow' | 'Normal' | 'Fast' = 'Normal';
 
     // Command that have no arguments (one-shot commands)
-    const commands_oneshot: Command[] = R.filter(R.complement(R.has('args')))(
-        commands,
-    ) as Command[];
-    // const commands_oneshot_piped: Command[] = R.compose(R.filter, R.complement, R.has('args'))(commands);
-    const commands_switches: Command[] = R.filter(
-        (cmd: Command) =>
-            R.has('args')(cmd) && cmd.args!.length === 1 && cmd.args![0].type === 'boolean',
-    )(commands) as ParametrizedCommand[];
-
-    const commands_switches_names: string[] = commands_switches.map(
-        (command) => command.command_name,
+    const CMD_actions: Command[] = R.filter((x: Command) => x.args.length == 0 && x.working)(
+        commands as Command[],
     );
+    // const CMD_actions_piped: Command[] = R.compose(R.filter, R.complement, R.has('args'))(commands);
+    const CMD_switches: Command[] = R.filter(
+        (cmd: Command) => cmd.working && cmd.args!.length === 1 && cmd.args![0].type === 'boolean',
+    )(commands as Command[]) as Command[];
+
+    const CMD_switches_names: string[] = CMD_switches.map((command) => command.command_name);
 
     onMount(() => {
         websocket.connect();
@@ -124,18 +181,18 @@
         websocket.send({ command: 'disconnect' });
     }
 
-    function handleModeSwitch(event: RadioGridChangeEvent) {
-        let mode = event.detail.value as string;
-        //index commands by command_name for easy lookup of the one that was triggered by the radio change
-        let commands = R.indexBy(R.prop('command_name'), commands_switches);
-        //we are interested in the api_code and the name of the arguments expected by the command
-        let command: ParametrizedCommand = commands[mode] as ParametrizedCommand;
-        let payload = {
-            api_id: command.api_code,
-            parameter: R.objOf(command.args?.[0]?.name, true),
-            topic: 'SPORT_MOD',
-        };
-        websocket.send(payload);
+    function handleModeSwitch(mode: string) {
+        //Look up the command triggered by the radio change
+        let cmd: Command | undefined = R.find((cmd) => cmd?.command_name == mode, CMD_switches);
+        //we are interested in the api_id and the name of the arguments expected by the command
+
+        if (cmd != undefined) {
+            websocket.send({
+                api_id: cmd.api_id,
+                parameter: R.objOf(cmd.args[0]?.name, true),
+                topic: cmd.topic,
+            });
+        }
     }
 
     function handleColorChange(event: RadioGridChangeEvent) {
@@ -146,8 +203,7 @@
             api_id: 1007,
             parameter: {
                 color: colorName.split(' ')[1],
-                time: 50,
-                flash_cycle: 1000,
+                time: 10,
             },
         });
     }
@@ -156,8 +212,30 @@
     }
 </script>
 
-<Video faceDetectionEnabled={FACE_DETECTION} faceLandmarksEnabled={FACE_LANDMARKS}/>
+<Video
+    faceDetectionEnabled={FACE_DETECTION}
+    faceLandmarksEnabled={FACE_LANDMARKS}
+    agePredictionEnabled={AGE_PREDICTION}
+/>
 <GamepadController monitoring={false} />
+<svelte:window
+    on:keydown|preventDefault={(event) => {
+        switch (event.key) {
+            case 'ArrowRight':
+                stepCounter = stepCounter >= actionSequence.length ? stepCounter : stepCounter + 1;
+                execute_step(actionSequence)(stepCounter);
+            // case 'ArrowLeft':
+            //     stepCounter -= 1;
+            //     execute_step(actionSequence)(stepCounter);
+            // case 'ArrowUp':
+            //     stepCounter += 1;
+            // case 'ArrowDown':
+            //     stepCounter -= 1;
+            // case 'Enter':
+            //     execute_step(actionSequence)(stepCounter);
+        }
+    }}
+/>
 <div class="ui-pane">
     <div>
         <Button
@@ -173,66 +251,70 @@
                 <p>Robot: {$robotConnected ? '✅' : '❌'}</p>
             </div>
         </Element>
-        <Switch label={"Monitor State"} callback ={
-            (value:boolean) =>  websocket.send({
+        <Switch
+            label={'Monitor State'}
+            value={false}
+            callback={(value: boolean) =>
+                websocket.send({
                     command: 'subscribe',
                     switch: value,
-                })
-        }/>
-         <Switch label={"Obstacle Avoidance"} callback ={
-            (value:boolean) =>  websocket.send({
+                })}
+        />
+        <Switch
+            value={true}
+            label={'Obstacle Avoidance'}
+            callback={(value: boolean) =>
+                websocket.send({
                     topic: 'OBSTACLES_AVOID',
                     api_id: 1001,
                     parameter: { enable: value },
-                })
-        }/>
-         <Switch label={"Face Landmarks"} callback ={
-            (value:boolean) => {FACE_LANDMARKS = value}
-        }/>
-         <Switch label={"Face Detection"} callback ={
-            (value:boolean) =>  {FACE_DETECTION = value}
-        }/>
-        <!-- <Checkbox
-            bind:value={OBSTACLE_AVOIDANCE}
-            label="Avoidance"
-            on:change={() =>
-                websocket.send({
-                    topic: 'OBSTACLES_AVOID',
-                    api_id: 1001,
-                    parameter: { enable: OBSTACLE_AVOIDANCE },
                 })}
-        /> -->
-        <!-- <Button
-            on:click={() =>
-                websocket.send({
-                    topic: 'OBSTACLES_AVOID',
-                    api_id: 1004,
-                    parameter: { is_remote_commands_from_api: true },
-                })}
-                title = "Remote over API"
         />
-        -->
+        <Switch
+            label={'Face Landmarks'}
+            callback={(value: boolean) => {
+                FACE_LANDMARKS = value;
+            }}
+        />
+        <Switch
+            label={'Face Detection'}
+            callback={(value: boolean) => {
+                FACE_DETECTION = value;
+            }}
+        />
+        <Switch
+            label={'Age Prediction'}
+            callback={(value: boolean) => {
+                AGE_PREDICTION = value;
+            }}
+        />
+        <Switch
+            label={'Gender Prediction'}
+            callback={(value: boolean) => {
+                GENDER_PREDICTION = value;
+            }}
+        />
     </div>
     <div>
-         <Button on:click={() => websocket.send({ command: 'oz_overture' })} title="Oz Overture" />
+        <Button on:click={() => websocket.send({ command: 'oz_overture' })} title="Oz Overture" />
         <Button on:click={() => websocket.send({ command: 'test' })} title="Test" />
         <Button on:click={() => websocket.send({ command: 'joystick' })} title="Test Joystick" />
     </div>
     <div class="button-grid">
-        {#each commands_oneshot as { api_code, working, command_name, topic }}
+        {#each CMD_actions as { api_id, working, command_name, topic }}
             {#if working}
                 <Button
                     on:click={() =>
-                        websocket.send({ api_id: api_code, command: command_name, topic: topic })}
+                        websocket.send({ api_id: api_id, command: command_name, topic: topic })}
                     title={command_name}
                 />
             {/if}
         {/each}
     </div>
     <RadioGrid
-        values={commands_switches_names}
+        values={CMD_switches_names}
         value={movementMode}
-        on:change={handleModeSwitch}
+        on:change={(e: RadioGridChangeEvent) => handleModeSwitch(e.detail.value as string)}
         label="Mode"
         columns={2}
     />
