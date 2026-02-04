fb89d51297122c247e4e892dd26a7ac878b732e7
martintricaud <martin.tricaud@gmail.com>
2026-02-05 00:11:16 +0100

    fixes style issues

--- unitree-go2-gui/src/components/Switch.svelte
+++ unitree-go2-gui/src/components/Switch.svelte
@@ -1,55 +1,58 @@
 <script lang="ts">
-    import * as R from 'ramda';
+    import * as R from 'ramda'
     export let value: boolean = false;
-    export let label: string = '';
-    export let callback: (a: boolean) => any = R.identity;
-    let baseCallback: (e: MouseEvent) => boolean = (e) => {
-        value = !value;
-        return value;
-    };
-    
+    export let label: string = "";
+    let baseCallback:(e:MouseEvent)=>boolean = (e) => {
+        value = !value
+        return value
+    }
+    export let callback: (a:boolean)=>any = R.identity
 </script>
+
 
 <div class="main">
     <div>{label}</div>
-    <button
-        aria-label="Basculer l'interrupteur"
-        aria-checked={value}
-        role="switch"
-        class="switch"
-        class:active={value}
-        on:click={R.pipe(baseCallback, callback)}
-    >
-        <div class="slider"></div>
-    </button>
+<button
+    aria-label="Basculer l'interrupteur"
+    aria-checked={value}
+    role="switch"
+    class="switch"
+    class:active={value}
+    on:click={R.pipe(baseCallback, callback)}>
+    <div class="slider"></div>
+</button>
 </div>
 
+
 <style>
-    .main {
+    .main{
         display: flex;
         gap: 1em;
         align-items: center;
         justify-content: space-between;
     }
     :root {
-        --switch-width: 3em;
-        --switch-height: calc(var(--switch-width) / 2);
-        --slider-size: calc(var(--switch-height) - 4px);
-        --switch-padding: 2px;
+        --switch-width: 48px;
+        --switch-height: 26px;
+        --slider-size: 26px;
+        --switch-padding: 0px;
         --switch-bg-color-off: #ccc;
         --switch-bg-color-on: #4caf50;
         --slider-color: white;
     }
 
     .switch {
+        font-size: 0;
         flex-shrink: 0;
         position: relative;
         width: var(--switch-width);
-        height: var(--switch-height);
-        padding: var(--switch-padding);
+        line-height: 1;
+        height: 24px;
+        padding: 0!;
         background-color: var(--switch-bg-color-off);
-        border: none;
-        border-radius: var(--switch-height);
+        border-width: 3px;
+        border-style: inset;
+        border-radius: 12px;
         cursor: pointer;
         transition: background-color 0.3s;
     }
@@ -59,18 +62,20 @@
     }
 
     .slider {
-        position: absolute;
-        width: var(--slider-size);
-        height: var(--slider-size);
-        background-color: var(--slider-color);
-        border-radius: 50%;
-        top: 50%;
-        transform: translateY(-50%);
-        left: var(--switch-padding);
+        background-color: rgb(230, 230, 230);
+        position: relative;
+        left:-1px;
+        top: -1px;
+        width: calc(var(--slider-size) - 6px);
+        height: calc(var(--slider-size) - 6px);
+        /* border-style: outset;
+        border-width: 3px; */
+        border: 1px solid #999;
+        border-radius: 12px;
         transition: left 0.3s;
     }
 
     .switch.active .slider {
-        left: calc(var(--switch-width) - calc(var(--slider-size)) - var(--switch-padding));
+        left: calc(var(--switch-width) - var(--slider-size) - var(--switch-padding));
     }
 </style>
