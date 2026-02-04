fbe5807372c13563b902fe8b74f7259dd66823f3
martintricaud <martin.tricaud@gmail.com>
2026-02-05 00:12:24 +0100

    code cleanup and renaming of variables for more consistency

--- unitree-go2-gui/src/components/Radio.svelte
+++ unitree-go2-gui/src/components/Radio.svelte
@@ -1,29 +1,27 @@
 
 <script lang="ts">
-    // The logic of the radio-button component should be implemented in a comonadic way, i.e. as a contextful computation.
-
     import * as R from 'ramda';
     import { onMount } from 'svelte';
     import type {Writable } from 'svelte/store'
     type RadioGroupData = {
+        currentOption: string;
+        // uncomment the line below to store the optionNames of all radiobuttons that target this store
         options: any[];
-        focus: string;
     }
     export let optionName: string = '';
     export let radioGroupData:Writable<RadioGroupData>
     export let callback: (a: string) => any = R.identity;
     let baseCallback = (ev:MouseEvent) => {
-        radioGroupData.update(R.assoc('focus', optionName))
+        radioGroupData.update(R.assoc('currentOption', optionName))
         return optionName;
     }
-    //appends an option with the given name to a list of options
-    let appendOption = (_optionName:string) => R.modify('options',R.append(_optionName))
     onMount(() => {
-        radioGroupData.update(appendOption(optionName))
-    });
+        // Uncomment the line below to store the optionNames of all radiobuttons that target this store
+        // radioGroupData.update(R.over(R.lensProp('options'), R.append(optionName)))
+    })
 </script>
-
+<!-- The effect of clicking the button is given by the composition of the baseCallback and the custom callback passed as prop upon instanciation-->
 <button on:click = {R.pipe(baseCallback, callback)}>
     {optionName}
-    {#if $radioGroupData.focus === optionName}✅{/if}
+    {#if $radioGroupData.currentOption === optionName}✅{/if}
 </button>
